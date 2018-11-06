6df9255d99b43db151d811364c56d28a0f619d4d
Jakub Brockl <brockljakub@gmail.com>
2018-11-06 19:52:53 +0100

    feat: Create index.php wp-theme

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -8,16 +8,26 @@ export class Slider extends React.PureComponent {
     public render() {
 
         return (
-            <Grid>
-                <Row>
-                    <Col xs={12} md={8}>
-                        <SliderDescription />
-                    </Col>
-                    <Col xs={6} md={4}>
-                        <SliderPosts />
-                    </Col>
-                </Row>
-            </Grid>
+            <div>
+                <style type="text/css">
+                    {`
+                    .slider {
+                        background-image: url("https://d39-a.sdn.szn.cz/d_39/c_img_F_N/wYsWL.jpeg?fl=cro,185,46,1699,955|res,450,,3|jpg,80");
+                        color: white;
+                    }
+                    `}
+                </style>
+                <Grid>
+                    <Row className="slider">
+                        <Col xs={12} md={8}>
+                            <SliderDescription />
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <SliderPosts />
+                        </Col>
+                    </Row>
+                </Grid >
+            </div>
         );
 
     }
