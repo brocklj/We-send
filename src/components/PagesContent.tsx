f9e9c6a901f98d371c69607a15fdfec6f0d4ad49
Jakub Brockl <brockljakub@gmail.com>
2018-11-09 20:47:06 +0100

    Add: Title into PagesContent

--- src/components/PagesContent.tsx
+++ src/components/PagesContent.tsx
@@ -30,8 +30,14 @@ export class PagesContent extends React.PureComponent<IPageContentProps, IPageCo
 
         return (
             <Page>
+                <h1></h1>
                 {data.map((item: any, i: number) => {
-                    return (<div key={i} dangerouslySetInnerHTML={{ __html: item.content.rendered }} />);
+                    return (
+                        <div>
+                            <h1>{item.title.rendered}</h1>
+                            <div key={i} dangerouslySetInnerHTML={{ __html: item.content.rendered }} />
+                        </div>
+                    );
                 })}
             </Page>
         );
